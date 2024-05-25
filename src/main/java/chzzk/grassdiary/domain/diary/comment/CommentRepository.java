45e74b99c21b8022d7f043ff1c78d1f2e02dc314
chjcoder <chjcoder@gmail.com>
2024-05-25 21:54:24 +0900

    fix(comment) : commentRepository 오타 수정

--- src/main/java/chzzk/grassdiary/domain/diary/comment/CommentRepository.java
+++ src/main/java/chzzk/grassdiary/domain/diary/comment/CommentRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.w3c.dom.stylesheets.LinkStyle;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findAllByDiary(Long diaryId);
+    List<Comment> findAllByDiaryId(Long diaryId);
 }
