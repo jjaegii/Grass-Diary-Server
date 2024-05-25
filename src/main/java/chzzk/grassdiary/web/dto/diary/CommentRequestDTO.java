7ebc771adad537cf5c85f6075a39435d3e2fde89
chjcoder <chjcoder@gmail.com>
2024-05-25 18:33:58 +0900

    feat(comment) : CommentRequestDTO 생성

---
+++ src/main/java/chzzk/grassdiary/web/dto/diary/CommentRequestDTO.java
@@ -0,0 +1,22 @@
+package chzzk.grassdiary.web.dto.diary;
+
+import chzzk.grassdiary.domain.diary.Diary;
+import chzzk.grassdiary.domain.diary.comment.Comment;
+import chzzk.grassdiary.domain.member.Member;
+
+public record CommentRequestDTO(
+        Long memberId,
+        Long diaryId,
+        String content,
+        Long parentCommentId
+) {
+    public Comment toEntity(Member member, Diary diary, Comment parentComment) {
+        return Comment.builder()
+                .member(member)
+                .diary(diary)
+                .content(content)
+                .parentComment(parentComment)
+                .build();
+    }
+}
+
