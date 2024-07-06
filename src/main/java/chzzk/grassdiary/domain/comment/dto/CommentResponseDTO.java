a99498c242930dea59ec30497a470774fdcd8e60
chjcoder <chjcoder@gmail.com>
2024-07-06 13:32:12 +0900

    fix(comment) : CommentResponseDTO에 CommentID값을 같이 보내주도록 추가

--- src/main/java/chzzk/grassdiary/domain/comment/dto/CommentResponseDTO.java
+++ src/main/java/chzzk/grassdiary/domain/comment/dto/CommentResponseDTO.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 public record CommentResponseDTO(
         // 멤버 정보(사진, 아이디), 댓글내용, 작성 시간
         // todo: 사진정보 추가
+        Long commentId,
         Long memberId,
         String content,
         boolean deleted,
@@ -17,6 +18,7 @@ public record CommentResponseDTO(
 ) {
     public static CommentResponseDTO from(Comment comment) {
         return new CommentResponseDTO(
+                comment.getId(),
                 comment.getMember().getId(),
                 comment.getContent(),
                 comment.isDeleted(),
@@ -28,6 +30,7 @@ public record CommentResponseDTO(
 
     public static CommentResponseDTO fromDeleted(Comment comment) {
         return new CommentResponseDTO(
+                comment.getId(),
                 null,
                 null,
                 true,
