d03671ef78871d78d1e506826594790a5ad88dee
HongYeseul <ghd8119@naver.com>
2024-07-24 23:45:33 +0900

    refactor(DiaryPreviewDTO): 일기 미리보기를 위한 DTO 클래스 간소화

--- src/main/java/chzzk/grassdiary/domain/diary/dto/browse/DiaryPreviewDTO.java
+++ src/main/java/chzzk/grassdiary/domain/diary/dto/browse/DiaryPreviewDTO.java
@@ -5,7 +5,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 import org.springframework.data.domain.Page;
 
-public record Top10DiariesDto(
+public record DiaryPreviewDTO(
         Long diaryId,
         String diaryContent,
         int diaryLikeCount,
@@ -13,9 +13,9 @@ public record Top10DiariesDto(
         String nickname,
         String createdAt) {
 
-    public static List<Top10DiariesDto> of(Page<Diary> diaries) {
+    public static List<DiaryPreviewDTO> of(Page<Diary> diaries) {
         return diaries.stream()
-                .map(d -> new Top10DiariesDto(
+                .map(d -> new DiaryPreviewDTO(
                         d.getId(),
                         d.getContent(),
                         d.getDiaryLikes().size(),
@@ -25,4 +25,23 @@ public record Top10DiariesDto(
                                 .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
                 )).toList();
     }
+
+    private static DiaryPreviewDTO toPreviewDiary(Diary diary) {
+        return new DiaryPreviewDTO(
+                diary.getId(),
+                diary.getContent(),
+                diary.getDiaryLikes().size(),
+                diary.getMember().getId(),
+                diary.getMember().getNickname(),
+                diary.getCreatedAt()
+                        .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+        );
+    }
+
+    public static List<DiaryPreviewDTO> toLatestDiariesDto(List<Diary> diaries) {
+        return diaries.stream()
+                .map(DiaryPreviewDTO::toPreviewDiary)
+                .toList();
+    }
+
 }
