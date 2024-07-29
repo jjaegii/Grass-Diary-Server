414e32335375c1ea98bd1e89e3b0c40007a679e1
HongYeseul <ghd8119@naver.com>
2024-07-29 09:02:55 +0900

    refactor(*): api 필드명 통일 - diaryContent → content - nickName → nickname

--- src/main/java/chzzk/grassdiary/domain/member/dto/MemberInfoDTO.java
+++ src/main/java/chzzk/grassdiary/domain/member/dto/MemberInfoDTO.java
@@ -2,7 +2,7 @@ package chzzk.grassdiary.domain.member.dto;
 
 public record MemberInfoDTO(
         String profileImageURL,
-        String nickName,
+        String nickname,
         String profileIntro
 ) {
 }
