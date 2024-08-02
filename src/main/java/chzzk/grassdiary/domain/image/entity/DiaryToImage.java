26d427a29a5480dd178288e53e3ba868a3983170
HongYeseul <ghd8119@naver.com>
2024-08-02 10:34:40 +0900

    refactor(image): 이미지 DB 구조 변경으로 인한 엔티티 클래스 수정 - DiaryImage 테이블 삭제 - DiaryToImage, Image 테이블 추가     - 이미지 주소 테이블과 이미지-일기간 매핑 테이블

--- src/main/java/chzzk/grassdiary/domain/image/entity/DiaryToImage.java
+++ src/main/java/chzzk/grassdiary/domain/image/entity/DiaryToImage.java
@@ -1,5 +1,6 @@
 package chzzk.grassdiary.domain.image.entity;
 
+import chzzk.grassdiary.domain.base.BaseCreatedTimeEntity;
 import chzzk.grassdiary.domain.diary.entity.Diary;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -8,7 +9,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,26 +19,27 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @Entity
-public class DiaryImage {
+public class DiaryToImage {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "diaryImage_id")
+    @Column(name = "diary_to_image_id")
     private Long id;
 
     @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "image_id")
+    private Image image;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "diary_id")
     private Diary diary;
 
-    @Lob
-    private String imagePath;
-
     @Builder
-    public DiaryImage(Diary diary, String imagePath) {
+    public DiaryToImage(Diary diary, Image image) {
         this.diary = diary;
-        this.imagePath = imagePath;
+        this.image = image;
     }
 
-    public void updateImagePath(String imagePath) {
-        this.imagePath = imagePath;
+    public void update(Image image) {
+        this.image = image;
     }
 }
