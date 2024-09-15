3a5c052450ea0f27ecd7a77ca692d6b387f517ae
HongYeseul <ghd8119@naver.com>
2024-09-15 15:57:07 +0900

    fix(ImageDTO): S3 URL 수정

--- src/main/java/chzzk/grassdiary/domain/image/dto/ImageDTO.java
+++ src/main/java/chzzk/grassdiary/domain/image/dto/ImageDTO.java
@@ -7,7 +7,7 @@ public record ImageDTO (
         Long imageId,
         String imageURL
 ) {
-    private static String baseURL = "https://chzzk-image-server.s3.ap-northeast-2.amazonaws.com/";
+    private static String baseURL = "https://chzzk-image-server-release.s3.ap-northeast-2.amazonaws.com/";
 
     public static ImageDTO from(DiaryToImage diaryToImage) {
         return new ImageDTO(diaryToImage.getImage().getId(), baseURL + diaryToImage.getImage().getImagePath());
